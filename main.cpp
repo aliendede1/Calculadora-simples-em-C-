5b991362f976eb4ee7927ce07aae1a4c8bd96df4
aliendede1 <manecreftandre@gmail.com>
2025-10-01 14:52:56 -0300

    Baskara ADD

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(){
-  int v1 = 0;
-  int v3 = 0;
-  int v2 = 0;
+  double v1 = 0;
+  double v3 = 0;
+  double v2 = 0;
   char op;
   int rel;
   cout << "Valores" << endl;
@@ -13,7 +13,7 @@ int main(){
   cin >> v1;
   cout << "B = ";
   cin >> v2;
-  cout << "Soma: + | Sub: - | Multiplicação: * | Divizão: / | Delta: d |  \n";
+  cout << "Soma: + | Sub: - | Multiplicação: * | Divizão: / | Delta: d | Baskara: B \n";
   cout << "Operador: ";
   cin >> op;
   switch (op){
@@ -43,6 +43,20 @@ int main(){
         cin >> v3;
         delta = (v2*v2)-(4*v1*v3);
         cout << delta << endl;
+          break;
+      case 'B':
+          double delt;
+          double x1,x2;
+          cout << "C = ";
+          cin >> v3;
+          delt = (v2*v2)-(4*v1*v3);
+          double raiz = sqrt(delt);
+          x1 = (-v2 + raiz)/(2*v1);
+          x2 = (-v2 - raiz)/(2*v1);
+          cout << "Delta: " << delt << endl;
+          cout << "X1: " << x1 << endl;
+          cout << "X2: " << x2 << endl;
+          cout << endl;
           break;
   }
   return 0;
